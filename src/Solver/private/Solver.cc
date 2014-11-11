31649a91799759333b4ed912373bbdc64e4b8457
Yimin <zhongym8903@gmail.com>
2014-11-11 16:32:56 -0600

    restore to previous version of solver.
    considering abandon generic gmres solver. Although iluapck is slow, we
    would definite need it for now.

--- src/Solver/private/Solver.cc
+++ src/Solver/private/Solver.cc
@@ -10,65 +10,8 @@
 namespace MEX {
 
 Solver::Solver() {
-	// default solver settings
-	option._type = Solver_type::direct;
-	option._restol = 1e-12;
-	option._multilevel = false;
-	option._nrestart = 30;
-	option._maxIter = 500;
-	option._smoother = Smoother::gsb;
-	option._amg = AMG::ilu;
+
 }
-
-
-void Solver::set_amg(int32_t _amg){
-	option._amg  = static_cast<AMG>(_amg);
-}
-
-void Solver::set_maxIter(int32_t _maxIter){
-	option._maxIter = _maxIter;
-}
-
-void Solver::set_nrestart(int32_t _nrestart) {
-	option._nrestart = _nrestart;
-}
-
-void Solver::set_restol(Real_t _restol) {
-	option._restol = _restol;
-}
-
-void Solver::set_smoother(int32_t _smoother){
-	option._smoother = static_cast<Smoother>(_smoother);
-}
-
-void Solver::set_type(int32_t _type) {
-	option._type = static_cast<Solver_type>(_type);
-}
-
-
-int32_t Solver::get_amg() {
-	return static_cast<int32_t>(option._amg);
-}
-int32_t Solver::get_maxIter() {
-	return option._maxIter;
-}
-
-int32_t Solver::get_nrestart(){
-	return option._nrestart;
-}
-
-Real_t Solver::get_restol() {
-	return option._restol;
-}
-
-int32_t Solver::get_smoother(){
-	return static_cast<int32_t>(option._smoother);
-}
-
-int32_t Solver::get_type() {
-	return static_cast<int32_t>(option._type);
-}
-
 
 Solver::~Solver() {
 #ifdef DEBUG
