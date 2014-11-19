40d0a338d0b695d2257036e6335f419f0af9ffc2
Yimin <zhongym8903@gmail.com>
2014-11-19 12:12:20 -0600

    Create PDEcs module instead a class for the project.
     It won't be faster, however, it is possible to make some assembly
    process faster, especially when considering multilevel
    mapping/projection.
     
     Now just a placehold here. it is a v1.2.0 addon.
    
    Signed-off-by: Yimin <zhongym8903@gmail.com>

---
+++ src/PDEcs/private/PDEcs.cpp
@@ -0,0 +1,21 @@
+/*
+ * PDEcs.cpp
+ *
+ *  Created on: Nov 19, 2014
+ *      Author: lurker
+ */
+
+#include "PDEcs.h"
+
+namespace MEX {
+
+PDEcs::PDEcs() {
+	// TODO Auto-generated constructor stub
+
+}
+
+PDEcs::~PDEcs() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace MEX */
