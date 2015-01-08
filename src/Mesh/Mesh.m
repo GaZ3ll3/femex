4441bd995592c4b1c9b2ee6f5cbcaf31b9ab3957
Yimin <GaZ3ll3@users.noreply.github.com>
2015-01-08 01:19:59 -0600

    Update Mesh.m
    indent

--- src/Mesh/Mesh.m
+++ src/Mesh/Mesh.m
@@ -1,36 +1,36 @@
 classdef Mesh < handle
 
 properties (Access = private)
-id_ % ID of the session instance.
+  id_ % ID of the session instance.
 end
 
 methods
 function this = Mesh(boundary, min_area)
-assert(isnumeric(boundary));
-this.id_ = Mesh_('new', boundary, min_area);
+  assert(isnumeric(boundary));
+  this.id_ = Mesh_('new', boundary, min_area);
 end
 
 function delete(this)
-Mesh_('delete',this.id_);
+  Mesh_('delete',this.id_);
 end
   
 function clear(this)
-assert(isscalar(this));
-Mesh_('clear', this.id_);
+  assert(isscalar(this));
+  Mesh_('clear', this.id_);
 end
 
 function report(this, verbose)
-assert(isscalar(this));
-Mesh_('report', this.id_); 
+  assert(isscalar(this));
+  Mesh_('report', this.id_); 
 end
   
 function [nodes, elems, segs, inds] = promote(this, degree)
-assert(isscalar(this));
-[nodes, elems, segs, inds] = Mesh_('promote', this.id_, degree);
+  assert(isscalar(this));
+  [nodes, elems, segs, inds] = Mesh_('promote', this.id_, degree);
 end
 
 function [np, nt, ne] = export(this)
-[np, nt, ne] = Mesh_('export', this.id_);
+  [np, nt, ne] = Mesh_('export', this.id_);
 end
   
   
