0906a1bdcf3d7a411e4aeee3dd3af9d9253ec8c1
Yimin <zhongym8903@gmail.com>
2015-05-05 22:50:01 -0500

    begin work on DG.
    Signed-off-by: Yimin <zhongym8903@gmail.com>

---
+++ src/DG/private/DG.h
@@ -0,0 +1,31 @@
+/*
+ * DG.h
+ *
+ *  Created on: May 3, 2015
+ *      Author: lurker
+ */
+
+#ifndef SRC_DG_PRIVATE_DG_H_
+#define SRC_DG_PRIVATE_DG_H_
+
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <cmath>
+
+#include <iostream>
+#include <iterator>
+#include <string.h>
+
+#include <mexplus.h>
+#include <pprint.h>
+
+#include "utils.h"
+
+class DG {
+public:
+	DG();
+	virtual ~DG();
+};
+
+#endif /* SRC_DG_PRIVATE_DG_H_ */
